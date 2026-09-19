c0853ad6eea9ce90aed01949fa0aac9c2e295221
livastic-vm4-primary-20260911 <onecursor9+livastic+vm4+primary+20260911@gmail.com>
2026-09-19 11:34:38 +0900

    Cleanup VM4 R41 playwright.config.mjs

--- playwright.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@playwright/test';
-import { withMergify } from '@mergifyio/playwright';
-export default withMergify(defineConfig({
-  testDir: './tests',
-  workers: 1,
-  reporter: [['list']],
-}));
